b0cf3c5d12733458031312f0e950f8ebff1fef4c
Gud184 <gudgupta8683@gmail.com>
2021-02-18 22:02:14 +0545

    django updated

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,13 @@ from .views import *
 app_name = "home"
 urlpatterns = [
 
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
+    path('blog', blog, name='blog'),
+    path('elements', elements, name='elements'),
+    path('blogsingle', blogsingle, name='blogsingle'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html',)
+
+def about(request):
+    return render(request, 'about.html',)
+
+def contact(request):
+    return render(request, 'contact.html',)
+
+def portfolio(request):
+    return render(request, 'portfolio.html',)
+
+def price(request):
+    return render(request, 'price.html',)
+
+def services(request):
+    return render(request, 'services.html',)
+
+def blog(request):
+    return render(request, 'blog-home.html')
+
+def elements(request):
+    return render(request, 'elements.html')
+
+def blogsingle(request):
+    return render(request, 'blog-single.html')
